23387abbbb95063fba91803d8f531aef27ed7621
Aaron Lin <aaron.l.lin.general@gmail.com>
2024-03-12 18:26:45 -0400

    fixed typo

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function introduction(name) {
 function introductionWithLanguage(name, language) {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
-function introductionWithLanguageOptional(name, language='Javascript') {
+function introductionWithLanguageOptional(name, language='JavaScript') {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
+console.log(introductionWithLanguageOptional('Gracie'));
